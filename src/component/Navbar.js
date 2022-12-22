31de03b0e2326b230b48a646574fc80f631a0168
Abdul Mufid <110360966+eabdalmufid@users.noreply.github.com>
2022-12-22 20:33:19 +0700

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -72,16 +72,16 @@ class NavBars extends Component {
             <h5>IslamKu.Id</h5>
 
             {/* <h4>Fitur Tersedia</h4> */}
-            <Link onClick={this.handleKlik} to="/react_quran/">
+            <Link onClick={this.handleKlik} to="/quran/">
               Home
             </Link>
-            <Link onClick={this.handleKlik} to="/react_quran/surah/">
+            <Link onClick={this.handleKlik} to="/quran/surah/">
               Al Quran
             </Link>
-            <Link onClick={this.handleKlik} to="/react_quran/story/">
+            <Link onClick={this.handleKlik} to="/quran/story/">
               Kisah Nabi
             </Link>
-            <Link onClick={this.handleKlik} to="/react_quran/pray/">
+            <Link onClick={this.handleKlik} to="/quran/pray/">
               Doa Doa Sehari Hari
             </Link>
 
